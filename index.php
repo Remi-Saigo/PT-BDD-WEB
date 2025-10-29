15901008cd7e6c3b8eda06f7e0c12783fd7e6068
Axel <axeldidier.boussou@ecoles-epsi.net>
2025-10-29 16:24:45 +0100

    info: ajout branche Axel

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
     //var_dump($_GET); (fonction qui permet d'afficher le contenu d'une variable, y compris les tableaux)
-    // Test
+    // Test Axel
     require_once 'header.php';   
     require_once 'db/mariadb.php';
 ?>
