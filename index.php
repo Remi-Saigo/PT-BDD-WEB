1cbe704df7f3a971db45d6c13fa850f0cc19c71d
Remi <legrandsoleil62@gmail.com>
2025-11-17 14:03:44 +0100

    init du projet

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     //var_dump($_GET); (fonction qui permet d'afficher le contenu d'une variable, y compris les tableaux)
     require_once 'header.php';   
+    require_once 'db/mariadb.php';
 ?>
 <?php
 if($dbh!=NULL){
