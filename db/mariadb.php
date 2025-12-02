ff1a44befeb7556361d0825c2e0698f2446b7774
Remi-Saigo <legrandsoleil62@gmail.com>
2025-12-02 11:13:23 +0100

    reload

--- db/mariadb.php
+++ db/mariadb.php
@@ -2,7 +2,7 @@
 //essaye de faire le code qui est dans le bloc try
 try {
     // se connecte à la BD et stocke la connexion dans  $dbh
-    $dbh = new PDO('mysql:host=localhost;dbname=PHPCours', 'login4439', 'HNLCQSaIAXkvUJo');
+    $dbh = new PDO('mysql:host=localhost;dbname=PTBDDWEB', 'login4439', 'HNLCQSaIAXkvUJo');
 } catch (PDOException $e) {
     //comme la connexion n'a pas fonctionnée , je stocke NULL dans $dbh
     $dbh=null;
